7cc85c0c1d144211ed89a68145e43ccbc91378f4
Avinash <avin.roy001@gmail.com>
2024-04-29 15:38:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const search = window.location.search;
+  const urlParams = new URLSearchParams(search);
+
+  const page = urlParams.get("page");
+  const results = urlParams.get("results");
+  const seed = urlParams.get("seed");
+
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true); 
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`https://randomuser.me/api/?page=${page}&results=${results}&seed=${seed}`);
+        setData(res.data.results);
+        console.log(res.data);
+        setIsLoading(false); 
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setIsLoading(false); 
+      }
+    };
+    fetchData();
+  }, [page, results, seed]);
+
+  console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {isLoading ? (
+        <div className='load'><p>Loading...</p></div>
+      ) : (
+        <div className='wrapper'>
+          {data && data.map((card, index) => (
+            <div className='cardWrapper' key={index}>
+              <div>
+                <img src={card.picture.large} alt='pic' /> 
+              </div>
+              <div className='block'>
+              <div className='fullname'>
+                <p>{card.name.first}</p> 
+                <p>{card.name.last}</p>
+              </div>
+              <div>
+                <p>{card.gender[0].toUpperCase()}{card.gender.slice(1)}</p>
+              </div>
+              <div>
+                <p>{card.phone}</p>
+              </div>
+              </div>
+            </div>
+          ))}
+          
+        </div>
+      )}
+      
     </div>
   );
 }
